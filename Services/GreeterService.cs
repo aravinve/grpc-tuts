e8cbbefa8b86e5af5ed50773821f374bb546bae2
Aravind Venkatesan <43500365+aravinve@users.noreply.github.com>
2024-02-23 10:00:07 +0800

    Initial Commit

---
+++ Services/GreeterService.cs
@@ -0,0 +1,27 @@
+using Grpc.Core;
+
+namespace grpc_tuts.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<ByeReply> SayBye(ByeRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new ByeReply{
+            Message = "Bye Bye " + request.Name + " !!!"
+        });
+    }
+}
