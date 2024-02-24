0aeef582ace9b93dec58cdce202b09aee8df4320
Aravind Venkatesan <43500365+aravinve@users.noreply.github.com>
2024-02-24 18:51:57 +0800

    Update Todo

--- Services/TodoService.cs
+++ Services/TodoService.cs
@@ -7,9 +7,8 @@ namespace grpc_tuts.Services;
 
 public class TodoService : Todo.TodoBase
 {
-
     private readonly ILogger<TodoService> _logger;
-    private Dictionary<string, TodoDto> _dictionary = new Dictionary<string, TodoDto>();
+    private static readonly Dictionary<string, TodoDto> _dictionary = new Dictionary<string, TodoDto>();
     public TodoService(ILogger<TodoService> logger)
     {
         _logger = logger;
@@ -25,7 +24,7 @@ public class TodoService : Todo.TodoBase
             IsCompleted = request.IsCompleted
         };
         _dictionary.Add(todo.Id, todo);
-        PrintTodoDict();
+        PrintTodoDict("Created TODO");
         return Task.FromResult(new CreateTodoResponse
         {
             Id = todo.Id,
@@ -34,13 +33,42 @@ public class TodoService : Todo.TodoBase
         });
     }
 
+    public override Task<UpdateTodoResponse> UpdateTodo(UpdateTodoRequest request, ServerCallContext context)
+    {
+        if (_dictionary.TryGetValue(request.Id, out var todoItem))
+        {
+            todoItem.Title = request.Title;
+            todoItem.IsCompleted = request.IsCompleted;
+            _dictionary.Remove(request.Id);
+            _dictionary.Add(todoItem.Id, todoItem);
+            PrintTodoDict("Updated TODO");
+            return Task.FromResult(new UpdateTodoResponse
+            {
+                Id = todoItem.Id,
+                Title = todoItem.Title,
+                IsCompleted = todoItem.IsCompleted
+            });
+
+        }
+        else
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "TodoId not found in the list."));
+        }
+    }
+
 
     #region Helper Methods
 
-    private void PrintTodoDict() {
+    private void PrintTodoDict(string action)
+    {
+        _logger.LogInformation(action);
+        if (_dictionary.Count <= 0)
+        {
+            _logger.LogInformation("Your Dict is Empty!!");
+        }
         foreach (var kvp in _dictionary)
         {
-            Console.WriteLine($"{kvp.Key}:{kvp.Value}");
+            _logger.LogInformation($"{kvp.Key}:{kvp.Value}");
         }
     }
 
